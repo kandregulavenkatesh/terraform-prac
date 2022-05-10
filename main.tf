f365389110031255d6d4b3b1864bb1fbe0a36905
venky <kandregulavenkatesh99@gmail.com>
2022-05-10 12:18:51 +0530

    added commit

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_security_group" "allow_all" {
      #ami = "${data.aws_ami.my_ami.id}"
      availability_zone = "us-east-1a"
      instance_type = "t2.micro"
-     key_name = "LaptopKey"
+     key_name = "keyboard-key"
      subnet_id = "${aws_subnet.subnet1-public.id}"
      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
      associate_public_ip_address = true	
